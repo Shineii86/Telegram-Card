dfd1ad9eb4eacdfb7430ee3b98f96fc1294a1141
Sʜɪɴᴇɪ Nᴏᴜᴢᴇɴ <157171073+Shineii86@users.noreply.github.com>
2026-02-21 12:41:54 +0530

    Add files via upload

--- types/enums.ts
+++ types/enums.ts
@@ -1,1 +1,17 @@
+/**
+ * Telegram Card - Enum Types
+ * https://github.com/Malith-Rukshan/telegram-card
+ * 
+ * Defines the various types of Telegram entities that can be displayed.
+ * 
+ * Copyright (c) 2025 Malith Rukshan
+ * Licensed under the MIT License
+ */
 
+export enum SoruceType {
+    Unknown = 'UNKNOWN',
+    User = 'USER',
+    Bot = 'BOT',
+    Channel = 'CHANNEL',
+    Group = 'GROUP',
+  }
